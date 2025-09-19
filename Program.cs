e33e1deb3e6c86fd2835dd9b13b1d70fe3e7d871
heloziza <helogiacometti@gmail.com>
2025-09-18 22:10:30 -0300

    docs adicionar explicação de tipo de dados

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@ Pessoa pessoa1 = new Pessoa();
 // nome de variável é Camel Case -> começa com minúscula e palavras compostas são escritas juntas sem espaços, com a primeira letra de cada palavra subsequentemente capitalizada
 // sem caractere especial nos nomes, apenas underline é aceito
 
+// tipos de dados:
+// string = série de caracteres
+// char = um único caractere
+// object = objeto
+// bool = booleano (true or false)
+// int = números inteiros (ele tem limite mas é alto)
+// uint = a diferença entre int e uint é que uint usa os 32 bits pra representar numeros positivos
+// long = inteiros que vão mais longe que int
+// ulong é de 64 bit (o dobro de uint e é só positivos)
+// byte vai de 0 a 255
+// para números decimais:
+// decimal é recomendável quando é dinheiro, é mais preciso quando se tem valores monetários
+// float é para os decimais como o int é para os inteiros
+// double é para os decimais como o long é para os inteiros
+// principais utilizados: string, object, bool, int, decimal e double
+
 pessoa1.Nome = "Helo";
 pessoa1.Idade = 20;
 pessoa1.Apresentar();
--- obj/Debug/net9.0/dotnetHelo.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnetHelo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetHelo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0dedda6e7232aec131747f3985053c2ec31fdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7522c9e820a40a0a84ef26168800062a059d626b")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetHelo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetHelo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
